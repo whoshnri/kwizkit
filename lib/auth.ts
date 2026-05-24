7afbdb6b20855b2a05079ec093e18621a7097fac
whoshnri <henrybassey2007@gmail.com>
2026-05-24 01:18:18 +0100

    feat: aded better auth dash

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,11 @@
 import { nextCookies } from "better-auth/next-js";
 import { betterAuth } from "better-auth";
+import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
   secret: process.env.BETTER_AUTH_SECRET,
-  plugins: [nextCookies()],
+  plugins: [nextCookies(), dash()],
   emailAndPassword: {
     enabled: true,
   },
