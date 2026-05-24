80cec884baf235065a2b572bc0b35a71a70952b4
whoshnri <henrybassey2007@gmail.com>
2026-05-24 01:43:04 +0100

    feat: added sentinel

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from "better-auth/client/plugins";
+import { sentinelClient } from "@better-auth/infra/client";
+
 
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ?? "http://localhost:3000",
-    plugins: [usernameClient()],
+    plugins: [usernameClient(), sentinelClient()],
 });
 
 export const { signIn, signUp, useSession, signOut } = authClient;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
 import { nextCookies } from "better-auth/next-js";
 import { betterAuth } from "better-auth";
-import { dash } from "@better-auth/infra";
+import { dash, sentinel } from "@better-auth/infra";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
   secret: process.env.BETTER_AUTH_SECRET,
-  plugins: [nextCookies(), dash()],
+  plugins: [nextCookies(), dash(), sentinel()],
   emailAndPassword: {
     enabled: true,
   },
